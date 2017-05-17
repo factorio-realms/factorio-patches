a11f02c814fcc72dfca10c96d8b03087d52eb2b5
Chizhong Jin <pjincz@gmail.com>
2017-05-18 04:38:48 +0800

    fixup gui left align on 0.15.11

--- patches/gui-left-vertical/script.lua
+++ patches/gui-left-vertical/script.lua
@@ -3,4 +3,7 @@ realm.patches.left_gui_vertical = {}
 realm.patches.left_gui_vertical.on_player_created = function(e)
   local player = game.players[e.player_index]
   player.gui.left.direction = 'vertical'
+  if mod_gui then
+    mod_gui.get_frame_flow(player).direction = 'vertical'
+  end
 end
