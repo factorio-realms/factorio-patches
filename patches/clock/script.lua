2975cf3c1dc37e90993e3b8286f9e191cc4e3a2c
Chizhong Jin <pjincz@gmail.com>
2017-05-18 16:21:54 +0800

    reduce clock cpu usage

--- patches/clock/script.lua
+++ patches/clock/script.lua
@@ -18,7 +18,7 @@ function clock_int_to_date(g)
   mm = (mi + 2)%12 + 1
   y = y + math.floor((mi + 2)/12)
   dd = ddd - math.floor((mi*306 + 5)/10) + 1
-  return y, mm, dd
+  return {y=y, m=mm, d=dd}
 end
 
 function clock_format_2digit(x)
@@ -29,22 +29,29 @@ function clock_format_2digit(x)
   end
 end
 
+CLOCK_LAST_G = nil
+CLOCK_LAST_YMD = nil
+
 function clock_update_gui()
-  local start = global.realm.clock_start_at
-  local g = clock_date_to_int(start[1], start[2], start[3])
-  g = g + math.floor((game.tick + 12500) / 25000)
-  local y, m, d = clock_int_to_date(g)
+  local g = global.realm.clock_start_at + math.floor((game.tick + 12500) / 25000)
+  local ymd
+  if g == CLOCK_LAST_G then
+    ymd = CLOCK_LAST_YMD
+  else
+    ymd = clock_int_to_date(g)
+    ymd.m = clock_format_2digit(ymd.m)
+    ymd.d = clock_format_2digit(ymd.d)
+    CLOCK_LAST_G = g
+    CLOCK_LAST_YMD = ymd
+  end
 
   local t = math.floor(((game.tick + 12500) % 25000) * 24 * 60 / 25000)
   local hh = math.floor(t / 60)
   local mm = t % 60
-
-  m = clock_format_2digit(m)
-  d = clock_format_2digit(d)
   hh = clock_format_2digit(hh)
   mm = clock_format_2digit(mm)
 
-  local str = {"patch-clock.yyyy-mm-dd-hh-mm", y, m, d, hh, mm}
+  local str = {"patch-clock.yyyy-mm-dd-hh-mm", ymd.y, ymd.m, ymd.d, hh, mm}
 
   for _, p in pairs(game.connected_players) do
     local root = mod_gui and mod_gui.get_frame_flow(p) or p.gui.left
@@ -64,7 +71,7 @@ end
 
 function realm.patches.clock.on_init()
   if not global.realm.clock_start_at then
-    global.realm.clock_start_at = {2067, 4, 1}
+    global.realm.clock_start_at = clock_date_to_int(2067, 4, 1)
   end
 end
 
