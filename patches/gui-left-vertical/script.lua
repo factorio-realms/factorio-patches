d14c559cf5be07c78b2612a523e63a68f7b58a42
Chizhong Jin <pjincz@gmail.com>
2017-05-21 06:37:48 +0800

    fixup: left_gui_vertical not works on an already exists map

--- patches/gui-left-vertical/script.lua
+++ patches/gui-left-vertical/script.lua
@@ -1,9 +1,19 @@
 realm.patches.left_gui_vertical = {}
 
-realm.patches.left_gui_vertical.on_player_created = function(e)
-  local player = game.players[e.player_index]
+function left_gui_vertical_patch(player)
   player.gui.left.direction = 'vertical'
   if mod_gui then
     mod_gui.get_frame_flow(player).direction = 'vertical'
   end
+end
+
+function realm.patches.left_gui_vertical.on_init()
+  for _, p in pairs(game.players) do
+    left_gui_vertical_patch(p)
+  end
+end
+
+function realm.patches.left_gui_vertical.on_player_created(e)
+  local player = game.players[e.player_index]
+  left_gui_vertical_patch(player)
 end
