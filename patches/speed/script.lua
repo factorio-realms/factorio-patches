f1b017e16656d89960d948d4dac2d3537b99b441
Chizhong Jin <pjincz@gmail.com>
2017-05-29 10:55:37 +0800

    add show speed function, update text

--- patches/speed/script.lua
+++ patches/speed/script.lua
@@ -3,6 +3,11 @@ realm.patches.speed = {
 }
 
 realm.patches.speed.commands.speed = function(e)
+  if #e.argv == 0 then
+    print_back(e, {"patch-speed.show", game.speed})
+    return
+  end
+
   if not e.by_admin then
     print_back(e, {"patch-speed.deny"})
     return
@@ -10,17 +15,12 @@ realm.patches.speed.commands.speed = function(e)
   local speed = tonumber(e.argv[1])
   if not speed then
     print_back(e, {"patch-speed.invalid"})
+    return
   end
   if not speed or speed < 0.05 or speed > 3 then
     print_back(e, {"patch-speed.out-of-range"})
     return
   end
   game.speed = speed
-  if e.player_index then
-    local player = game.players[e.player_index]
-    game.print{"patch-speed.updated", speed, player.name}
-  else
-    -- keep silent when speed is updated by console
-    print_to(-1, {"patch-speed.updated", speed, "console"})
-  end
+  game.print{"patch-speed.updated", speed, e.commander}
 end
